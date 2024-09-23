1f1c85e1e7af7d51e86cf5a9e9e7384d7f8425fe
GremiTiger032 <bgremi1@lsu.edu>
2024-09-23 08:16:03 -0500

    update

--- sketch.js
+++ sketch.js
@@ -8,8 +8,31 @@ function draw() {
 
 
 for(var i=5; i < 1000; i = i + 15){
+fill(51)
 ellipse(i, 10, 10);
-}
+fill(123, 1)
+ellipse(i, 50, 14);
+fill(51)
+ellipse(i, 100, 18);
+fill(123, 1)
+ellipse(i, 150, 22);
+fill(51)
+ellipse(i, 200, 26);
+fill(123, 1)
+ellipse(i, 250, 30);
+fill(51)
+ellipse(i, 300, 34);
+fill(123, 1)
+ellipse(i, 350, 38);
+fill(51)
+ellipse(i, 400, 42);
+fill(123, 1)
+ellipse(i, 450, 46);
+fill(1)
+rotate(25, 26)}
+
+
+       
 
 
 }
